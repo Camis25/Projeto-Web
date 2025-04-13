2ea055c85f37e3cfd8ed238552f657e0d4fbc2f3
Camis25 <camisvitoriarosasantos2005@gmail.com>
2025-04-13 16:40:38 -0300

    Parte visual

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,11 @@
+function loginUsuario(){
+    var textoEmail = document.getElementById('txtEmail').value;
+    var textoSenha = document.getElementById('txtSenha').value;
+
+    if(textoEmail == "Camile" && textoSenha == "1234"){
+        window.location.href = 'teste.html';
+    }else{
+        alert("Usuário ou senha incorretos");
+    }
+
+}
